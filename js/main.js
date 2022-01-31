27b3db83a78160f6a6bc784dd1eb0b0699911998
min-log <minlog.0430@gmail.com>
2022-01-31 17:29:17 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search'); //HTML 
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click',function(){
-    searchInputEl.focus();//focus 강재 적용
-});
-
-searchInputEl.addEventListener('focus',function(){
-    searchEl.classList.add('focused'); // 클레스 추가
-    searchInputEl.setAttribute('placeholder','통합검색')//html 속성 지정
-});
-searchInputEl.addEventListener('blur',function(){ 
-    //focus 가 제거 되면 
-    searchEl.classList.remove('focused'); // 클레스 추가
-    searchInputEl.setAttribute('placeholder','')//html 속성 지정
-});
-
 //header scroll animation  플러그인 사용
 const badgesEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -186,8 +169,3 @@ spyEls.forEach(function(spyEl){
         .setClassToggle(spyEl, 'show')//클레스 지정 setClassToggle
         .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date(); //오늘의 날짜 
-
